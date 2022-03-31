896eba4339ad170fc3cccc7551e486829db9ca97
David Ku <hola@davidku.dev>
2022-03-30 21:18:53 -0600

    se agrega el modelo de users para verificarcion

--- middleware/protectMiddleware.js
+++ middleware/protectMiddleware.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
-const User = require(""); // TODO Ruta de modelo de usuarios
+const User = require("../v1/models/usersModels");
 
 const protect = asyncHandler(async (req, res, next) => {
   let token;
